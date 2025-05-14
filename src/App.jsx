647d7738855b085bd4cb5158eadd5d801ebd8501
Stiven hoxha <stivenhoxha007@gmail.com>
2025-05-14 20:37:20 +0200

    milestone 1 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // 1 --> Creazione dell'array (MILESOTNE 1)
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 }
+  ];
+
+  // 4 --> CREO IL MIO STATE (MILESTONE 2)
+  const [addedProducts, setAddedProducts] = useState([]);
+
+  // 7 --> CREARE LA FUNZIONE DEL FUNZIONAMENTO DEL BOTTONE AL CLICK, CHE PRENDERA' COME IMPASTO IL NOSRTO PRODUCT CHE CHIAMEREMO PRODUCT (MILESTONE 2)
+  const addToCart = () => {
+    // IL NOSTRO PRODUCT DOVRA' AIUTARCI SE C'E' GIA' UN PRODOTTO O NO CON LO STESSO NOME, UTILIZZANDO SOME CHE RITORNO UN BOOLEANO (MIELSTONE 2)
+    const isProductAlreadyAdded = addedProducts.some(p => p.name === product.name);
+    if (isProductAlreadyAdded) {
+      return;
+    }
+    // 9 --> DICHIARANDO IL PRODOTTO NUOVO DEL PUNTO 9 (MILESTONE 2)
+    const productToAdd = {
+      ...product,
+      quantity: 1
+    }
+    // 8 --> SE IL RPODOTTO NON E' PRESENTE (MILESTONE 2)
+    setAddedProducts(curr => [...curr, productToAdd]);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* 2 --> CREAZIONE DEL COMPONENTE CHE MOSTRERA' LA LISTA DEI PRODOTTI (MILESOTNE 1) */}
+      <h1>Prodotti tra cui scegliere</h1>
+      {/* 3 --> FUNZIONE PER MOSTRARE I PRODOTTI IN UNA LISTA DI <li> (MILESOTNE 1) */}
+      <ul>
+        {products.map((p, i) => (
+          <li key={i}>
+            <p>{p.name} ({p.price.toFixed(2)})</p>
+            {/* 5 --> CREAZIONE DI UN BOTTONE PER OGNI PRODOTTO DELLA LISTA (MILESTONE 2) */}
+            {/* 6 --> INSERIMENTO DELLA FUNZIONE ADDTOCART AL CLICK PER AGGIUNGERE IL PRODOTTO (MILESTONE 2) */}
+            <button onClick={() => addToCart(p)}>Aggiugni al carello</button>
+          </li>
+        ))}
+      </ul>
+      {/* 10 -->  (MIELSTONE 3)*/}
+      {addedProducts.length > 0 && (<>
+        <h2>Carrello</h2>
+        <ul>
+          {addedProducts.map((p, i) => (
+            <li key={i}>
+              <p>{p.quantity} x {p.name} ({p.price.toFixed(2)})</p>
+            </li>
+          ))}
+        </ul>
+      </>)}
     </>
   )
 }
---
+++ src/file.md
@@ -0,0 +1,32 @@
+Obiettivo:
+Stai lavorando su un'applicazionedi e-commerce e devi implementare in sistema di gestione del carrello.
+
+MILESTONE 1: mostrare la lista dei prodotti
+1 --> Parti dall'array `products` fornito:
+const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 }
+];
+
+2 --> Crea un componente che mostra la lista dei prodotti
+3 --> Per ogni prodotto, mostra: 
+        - NOME
+        - PREZZO
+
+Obiettivo: Vedere un elenco leggibile di tutti i prodotti con nome e prezzo
+-------------------------------------------------------------------------
+MILESTONE 2: Aggiungere prodotti al carello
+
+1 --> Aggiugni uno stato locale `addedProducts` (inizzialmente un array vuoto) per rappresentare i prodotti nel carrello
+2 --> Per ogni prodotto della lista, aggiungi un bottone "Aggiugni al carrello":
+                        - Al click del bottone, usa una funzione `addToCart` per: 
+                        - Aggiungere il prodotto al carrello se non è già presente, con na prorpietà ``quantity = 1
+3 --> Sotto alla lista dei prodotti, mostra una lista dei prodotti nel carrello se `addeDProducts` contiene almeno un elemento 
+                        - Per ogni prodotto nel carello, mostra:
+                        - NOME
+                        - PREZZO
+                        - QUANTITA'
+
+OBIETTIVO: L'utente può aggiungere prodotti al carrello e vedere  una lista dei prodotti aggiunti
